ee8c30a4a5ea6578618105409d601c2f21a207d2
Viktor Mishustin <mrgyugo@gmail.com>
2015-08-16 22:24:38 +0300

    Удаление проекта Тест (reverted from commit 3844bee9a5d81f97f8ff24fadb59c66b742ae860)

---
+++ Test1/Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  Test1
+//
+//  Created by Viktor on 16.08.15.
+//  Copyright (c) 2015 Viktor. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+@end
---
+++ Test1/Test1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test1
+//
+//  Created by Viktor on 16.08.15.
+//  Copyright (c) 2015 Viktor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
