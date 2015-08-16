4df581f82fbb1a86faca01c83257b48c4bf66be4
Viktor Mishustin <mrgyugo@gmail.com>
2015-08-17 01:29:50 +0300

    Поправка репозитория

---
+++ StukorenkoProperties/ASBoxer.h
@@ -0,0 +1,19 @@
+//
+//  ASBoxer.h
+//  StukorenkoProperties
+//
+//  Created by Viktor on 17.08.15.
+//  Copyright (c) 2015 Viktor. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface ASBoxer : NSObject
+
+@property (strong, nonatomic) NSString* name;
+@property (assign, nonatomic) NSInteger age;
+@property (assign, nonatomic) CGFloat height;
+@property (assign, nonatomic) CGFloat weight;
+
+@end
---
+++ StukorenkoProperties/ASBoxer.m
@@ -0,0 +1,13 @@
+//
+//  ASBoxer.m
+//  StukorenkoProperties
+//
+//  Created by Viktor on 17.08.15.
+//  Copyright (c) 2015 Viktor. All rights reserved.
+//
+
+#import "ASBoxer.h"
+
+@implementation ASBoxer
+
+@end
