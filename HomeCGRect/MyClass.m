9dec74d85e13d7f7a6731b39a5da898a034c721d
Viktor Mishustin <mrgyugo@gmail.com>
2015-08-16 22:42:44 +0300

    Создание нового класса myClass

---
+++ HomeCGRect/MyClass.m
@@ -0,0 +1,13 @@
+//
+//  MyClass.m
+//  HomeCGRect
+//
+//  Created by Viktor on 16.08.15.
+//  Copyright (c) 2015 Viktor. All rights reserved.
+//
+
+#import "MyClass.h"
+
+@implementation MyClass
+
+@end
