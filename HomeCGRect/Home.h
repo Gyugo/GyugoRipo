4ebf7f1b5f9d9bb485ad90007c02586ac09b8abd
Viktor Mishustin <mrgyugo@gmail.com>
2015-08-16 22:30:34 +0300

    Создание проекта Home

---
+++ HomeCGRect/Home.h
@@ -0,0 +1,13 @@
+//
+//  Home.h
+//  HomeCGRect
+//
+//  Created by Viktor on 16.08.15.
+//  Copyright (c) 2015 Viktor. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Home : NSObject
+
+@end
---
+++ HomeCGRect/HomeCGRect/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HomeCGRect
+//
+//  Created by Viktor on 16.08.15.
+//  Copyright (c) 2015 Viktor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
