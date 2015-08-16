1c87bc487cfd69e1edda6ec91722b4bd37d1a23b
Viktor Mishustin <mrgyugo@gmail.com>
2015-08-16 22:45:58 +0300

    Создание класса main

---
+++ HomeCGRect/Main.h
@@ -0,0 +1,13 @@
+//
+//  Main.h
+//  HomeCGRect
+//
+//  Created by Viktor on 16.08.15.
+//  Copyright (c) 2015 Viktor. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Main : NSObject
+
+@end
---
+++ HomeCGRect/Main.m
@@ -0,0 +1,13 @@
+//
+//  Main.m
+//  HomeCGRect
+//
+//  Created by Viktor on 16.08.15.
+//  Copyright (c) 2015 Viktor. All rights reserved.
+//
+
+#import "Main.h"
+
+@implementation Main
+
+@end
