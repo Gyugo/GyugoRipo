8dadb5e126a70e93ae23ba34c9db0960b4b7980e
Viktor Mishustin <mrgyugo@gmail.com>
2015-08-19 13:53:38 +0300

    Изуччение работы массивов, внедрение в прект

---
+++ Company/HRDepartment.h
@@ -0,0 +1,13 @@
+//
+//  HRDepartment.h
+//  Company
+//
+//  Created by Viktor on 19.08.15.
+//  Copyright (c) 2015 Viktor. All rights reserved.
+//
+
+#import "CompanyMain.h"
+
+@interface HRDepartment : CompanyMain
+
+@end
---
+++ Company/HRDepartment.m
@@ -0,0 +1,13 @@
+//
+//  HRDepartment.m
+//  Company
+//
+//  Created by Viktor on 19.08.15.
+//  Copyright (c) 2015 Viktor. All rights reserved.
+//
+
+#import "HRDepartment.h"
+
+@implementation HRDepartment
+
+@end
