101f6b7d0438475447547098530d52371da7f478
Viktor Mishustin <mrgyugo@gmail.com>
2015-08-20 14:10:07 +0300

    Учусь инициализировать массив.

---
+++ MyArray/MyArray.m
@@ -0,0 +1,17 @@
+//
+//  MyArray.m
+//  MyArray
+//
+//  Created by Viktor on 20.08.15.
+//  Copyright (c) 2015 Viktor. All rights reserved.
+//
+
+#import "MyArray.h"
+
+@implementation MyArray
+
+- (void) initArray {
+    _mArray = [[NSMutableArray alloc] initWithObjects: nil];
+}
+
+@end
