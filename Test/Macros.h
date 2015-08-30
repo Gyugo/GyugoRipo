a697c6dfa8856da1d234e394cfa7287fbd45c2c7
Viktor Mishustin <mrgyugo@gmail.com>
2015-08-30 17:13:59 +0300

    add project

---
+++ Test/Macros.h
@@ -0,0 +1,13 @@
+//
+//  Macros.h
+//  Test
+//
+//  Created by Viktor on 30.08.15.
+//  Copyright (c) 2015 Viktor. All rights reserved.
+//
+
+#ifndef Test_Macros_h
+#define Test_Macros_h
+
+
+#endif
---
+++ Test/ViewControllerDetail.h
@@ -0,0 +1,13 @@
+//
+//  ViewControllerDetail.h
+//  Test
+//
+//  Created by Viktor on 30.08.15.
+//  Copyright (c) 2015 Viktor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewControllerDetail : UIViewController
+
+@end
