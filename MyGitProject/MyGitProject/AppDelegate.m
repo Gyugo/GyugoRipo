1400fda6ecad701761818b71af20613900adc773
Viktor Mishustin <mrgyugo@gmail.com>
2015-09-10 16:49:51 +0300

    Add hello

--- MyGitProject/MyGitProject/AppDelegate.m
+++ MyGitProject/MyGitProject/AppDelegate.m
@@ -16,7 +16,11 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    
+    
+    NSLog(@"Hello World");
+    
+    
     return YES;
 }
 
